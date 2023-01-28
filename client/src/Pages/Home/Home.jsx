a73e75a59481e26838b44a594e0b78ade216252a
Nathan <nathan.forman@gmail.com>
2023-01-27 16:32:17 -0800

    client Home file assembled

---
+++ client/src/Pages/Home/Home.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react"
+import Header from "../../components/Header/Header"
+import Posts from "../../components/Posts/Posts"
+import Sidebar from "../../components/Sidebar/Sidebar"
+import axios from "axios"
+import styled from "styled-components"
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts")
+      setPosts(res.data)
+    }
+    fetchPosts()
+  }, []);
+
+  return (
+    <StyledHome>
+    <Header/>
+    <div className="home">
+        <Posts posts={posts}/>
+        <Sidebar/>
+    </div>
+    </StyledHome>
+  )
+}
+
+const StyledHome = styled.div`
+    display: flex;
+`
