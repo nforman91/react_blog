6f3ee3fb4abd8dc2c65f08ba4542c3abdaef37a7
Nathan <nathan.forman@gmail.com>
2023-02-01 14:40:35 -0800

    Adding models and routes files to api

--- api/index.js
+++ api/index.js
@@ -36,6 +36,6 @@ app.use("/api/users", usersRoute);
 app.use("/api/posts", postsRoute);
 app.use("/api/categories", categoriesRoute);
 
-app.listen("8000", () => {
+app.listen("5000", () => {
     console.log("Backend is running.")
 });
